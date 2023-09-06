0fbbcaaaabcc877a798ff24114662f17afbd7976
Pablo Lima <140986925+PabloLimaa@users.noreply.github.com>
2023-09-05 21:04:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     let d = document.createElement('div');
-    d.classList = 'div__scroll__comentarios';
+    d.classList = 'div__scroll__comentarios Comment__post';
     d.innerHTML = `<h5><strong>${inputName.value}</strong></h5> <p>${textComment.value}</p> <img src="https://cdn-icons-png.flaticon.com/512/126/126468.png" width="15px" height="15px">`;
     Comment__post.appendChild(d);
     inputName.value = "";
