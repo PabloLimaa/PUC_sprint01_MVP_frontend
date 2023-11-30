c4704d56fa142222ef118806b1966e06618f8f04
Pablo Lima <140986925+PabloLimaa@users.noreply.github.com>
2023-11-30 12:45:24 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+## Single Page Aplication (Front-End)
+Este projeto faz parte do material didático da disciplina Desenvolvimento Full Stack Básico (Puc-Rio).
+
+O objetivo principal deste repositório é materializar o que foi lecionado durante a primeira sprint de front-end.
+
+Como Executar
+Para executar este projeto localmente, siga as instruções abaixo:
+
+Faça o download ou clone este repositório em sua máquina local:
+
+bash
+Copy code
+git clone https://github.com/seu-usuario/seu-projeto.git
+Navegue até o diretório do projeto:
+
+bash
+Copy code
+cd seu-projeto
+Abra o arquivo index.html em seu navegador preferido. Você pode simplesmente dar um duplo clique no arquivo ou usar o seguinte comando:
+
+bash
+Copy code
+open index.html
+Isso abrirá o projeto no seu navegador padrão.
+
+Contribuição
+Se você deseja contribuir para este projeto, siga as etapas abaixo:
+
+Faça um fork do projeto.
+Crie uma branch para sua contribuição: git checkout -b feature/nova-feature.
+Faça suas alterações e commit: git commit -m 'Adiciona nova feature'.
+Faça push para a branch: git push origin feature/nova-feature.
+Abra um pull request.
+Estamos ansiosos para receber suas contribuições!
+
+Contato
+Se você tiver alguma dúvida ou sugestão, sinta-se à vontade para entrar em contato:
+
+Nome: Pablo Henrique Costa de Lima
+E-mail: pablo.henriquelima07@gmail.com
+LinkedIn: https://www.linkedin.com/in/pablo-henrique-lima-542300164/
+Agradeço sua visita!
--- script.js
+++ script.js
@@ -1,23 +1,139 @@
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter a lista existente do servidor via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const getList = async () => {
+  let url = 'http://127.0.0.1:5000/postagens';
+  fetch(url, {
+    method: 'get',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.postagens.forEach(item => insertList(item.nome, item.comentario))
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
 
 /*
   --------------------------------------------------------------------------------------
-  Função para adicionar um novo comentário  
+  Chamada da função para carregamento inicial dos dados
   --------------------------------------------------------------------------------------
 */
+getList()
 
-const inputName = document.getElementById('input__nome');
-const textComment = document.getElementById('input__text');
-const form = document.getElementById('formulario');
-const Comment__post = document.getElementById('Comment__post');
+/*
+  --------------------------------------------------------------------------------------
+  Função para colocar uma postagem na lista do servidor via requisição POST
+  --------------------------------------------------------------------------------------
+*/
+const postItem = async (nome, comentario) => {
+  const formData = new FormData();
+  formData.append('nome', nome);
+  formData.append('comentario', comentario);
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+  let url = 'http://127.0.0.1:5000/postagem';
+  fetch(url, {
+    method: 'post',
+    body: formData
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
 
-    let d = document.createElement('div');
-    d.classList = 'div__scroll__comentarios Comment__post';
-    d.innerHTML = `<h5><strong>${inputName.value}</strong></h5> <p>${textComment.value}</p> <img src="https://cdn-icons-png.flaticon.com/512/126/126468.png" width="15px" height="15px">`;
-    Comment__post.appendChild(d);
-    inputName.value = "";
-    textComment.value = "";
-    inputName.focus();
-});
+/*
+  --------------------------------------------------------------------------------------
+  Função para criar um botão close para cada postagem da lista
+  --------------------------------------------------------------------------------------
+*/
+const insertButton = (parent) => {
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  parent.appendChild(span);
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para remover uma postagem da lista de acordo com o click no botão close
+  --------------------------------------------------------------------------------------
+*/
+const removeElement = () => {
+  let close = document.getElementsByClassName("close");
+  // var table = document.getElementById('myTable');
+  let i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement.parentElement;
+      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+      if (confirm("Você tem certeza?")) {
+        div.remove()
+        deleteItem(nomeItem)
+        alert("Removido!")
+      }
+    }
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para deletar uma postagem da lista do servidor via requisição DELETE
+  --------------------------------------------------------------------------------------
+*/
+const deleteItem = (item) => {
+  console.log(item)
+  let url = 'http://127.0.0.1:5000/postagem?nome=' + item;
+  fetch(url, {
+    method: 'delete'
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para adicionar uma nova postagem com nome e comentario 
+  --------------------------------------------------------------------------------------
+*/
+const newItem = () => {
+  let inputNome = document.getElementById("nome").value;
+  let inputComentario = document.getElementById("comentario").value;
+
+  if (inputNome === '') {
+    alert("Escreva seu nome!");
+  } else if (inputComentario === '') {
+    alert("Deixe um comentário sobre o vídeo!");
+  } else {
+    insertList(inputNome, inputComentario)
+    postItem(inputNome, inputComentario)
+    alert("Postagem adicionada!")
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para inserir items na lista apresentada
+  --------------------------------------------------------------------------------------
+*/
+const insertList = (nome, comentario) => {
+  var item = [nome, comentario]
+  var table = document.getElementById('myTable');
+  var row = table.insertRow();
+
+  for (var i = 0; i < item.length; i++) {
+    var cel = row.insertCell(i);
+    cel.textContent = item[i];
+  }
+  insertButton(row.insertCell(-1))
+  document.getElementById("nome").value = "";
+  document.getElementById("comentario").value = "";
+
+  removeElement()
+}
